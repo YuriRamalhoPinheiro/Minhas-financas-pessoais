d18c7375d2929e03788e114b5d3b925209d28cae
Yuri Ramalho Pinheiro <yuriramalhopinheiro@gmail.com>
2020-06-14 17:37:33 -0300

    adicionando lombok no projeto

--- src/main/java/br/com/yuri/minhasfinancas/MinhasFinancasApplication.java
+++ src/main/java/br/com/yuri/minhasfinancas/MinhasFinancasApplication.java
@@ -8,6 +8,7 @@ public class MinhasFinancasApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MinhasFinancasApplication.class, args);
+		
 	}
 
 }
--- src/main/java/br/com/yuri/minhasfinancas/model/entity/Lancamento.java
+++ src/main/java/br/com/yuri/minhasfinancas/model/entity/Lancamento.java
@@ -2,7 +2,6 @@ package br.com.yuri.minhasfinancas.model.entity;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Convert;
@@ -18,8 +17,11 @@ import javax.persistence.Table;
 
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
+import lombok.Data;
+
 @Entity
 @Table( name = "lancamento", schema = "financas")
+@Data
 public class Lancamento {
 	
 	@Id
@@ -47,98 +49,5 @@ public class Lancamento {
 	@Column(name = "status")
 	@Enumerated(EnumType.STRING)
 	private StatusLancamento statusLancamento;
-	
-	public Lancamento() {
-		super();
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Integer getMes() {
-		return mes;
-	}
-
-	public void setMes(Integer mes) {
-		this.mes = mes;
-	}
-
-	public Integer getAno() {
-		return ano;
-	}
-
-	public void setAno(Integer ano) {
-		this.ano = ano;
-	}
-
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
-
-	public BigDecimal getValor() {
-		return valor;
-	}
-
-	public void setValor(BigDecimal valor) {
-		this.valor = valor;
-	}
-
-	public LocalDate getDataDeCadastro() {
-		return dataDeCadastro;
-	}
-
-	public void setDataDeCadastro(LocalDate dataDeCadastro) {
-		this.dataDeCadastro = dataDeCadastro;
-	}
-
-	public TipoLancamento getTipoLancamento() {
-		return tipoLancamento;
-	}
-
-	public void setTipoLancamento(TipoLancamento tipoLancamento) {
-		this.tipoLancamento = tipoLancamento;
-	}
-
-	public StatusLancamento getStatusLancamento() {
-		return statusLancamento;
-	}
-
-	public void setStatusLancamento(StatusLancamento statusLancamento) {
-		this.statusLancamento = statusLancamento;
-	}
-
-	@Override
-	public String toString() {
-		return "Lancamento [id=" + id + ", mes=" + mes + ", ano=" + ano + ", usuario=" + usuario + ", valor=" + valor
-				+ ", dataDeCadastro=" + dataDeCadastro + ", tipoLancamento=" + tipoLancamento + ", statusLancamento="
-				+ statusLancamento + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(ano, dataDeCadastro, id, mes, statusLancamento, tipoLancamento, usuario, valor);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!(obj instanceof Lancamento))
-			return false;
-		Lancamento other = (Lancamento) obj;
-		return Objects.equals(ano, other.ano) && Objects.equals(dataDeCadastro, other.dataDeCadastro)
-				&& Objects.equals(id, other.id) && Objects.equals(mes, other.mes)
-				&& statusLancamento == other.statusLancamento && tipoLancamento == other.tipoLancamento
-				&& Objects.equals(usuario, other.usuario) && Objects.equals(valor, other.valor);
-	}
 	
 }
